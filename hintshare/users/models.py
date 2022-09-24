89f9ff15fcb8f544af979c45e430b7c0afe919b5
Dilu-Bilu <dilreetraju@gmail.com>
2022-09-24 13:21:10 -0700

    changed path to name

--- hintshare/users/models.py
+++ hintshare/users/models.py
@@ -36,11 +36,11 @@ class User(AbstractUser):
 
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
-        img = Image.open(self.Image.path)
+        img = Image.open(self.Image.name)
         if img.height > 300 or img.width > 300:
             output_size = (300, 300)
             img.thumbnail(output_size)
-            fh = storage.open(self.Image.path, "wb")
+            fh = storage.open(self.Image.name, "wb")
             format = 'jpg'  # You need to set the correct image format here
             img.save(fh, format)
             fh.close()
