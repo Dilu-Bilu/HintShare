63ba726a0d2e96c661083f21a3d5110b4f60252a
Dilu-Bilu <dilreetraju@gmail.com>
2022-09-24 15:53:28 -0700

    changed allowed hosts

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,14 +1,13 @@
 from .base import *  # noqa
 from .base import env
-
 # GENERAL
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["hintshare.ca"]) + ['hintshare.ca', 'www.hintshare.ca']
-
-# DATABASES
+# ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["hintshare.ca"]) + ['hintshare.ca', 'www.hintshare.ca']
+ALLOWED_HOSTS = ["hintshare.ca", "www.hintshare.ca", "hintshare.herokuapp.com"]
+# DATABASES kjbkbk
 # ------------------------------------------------------------------------------
 DATABASES["default"] = env.db("DATABASE_URL")  # noqa F405
 DATABASES["default"]["ATOMIC_REQUESTS"] = True  # noqa F405
