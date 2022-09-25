cfa95835555bcd6d89d278cf88dfc18db40543cd
Dilu-Bilu <dilreetraju@gmail.com>
2022-09-24 20:06:24 -0700

    changed back settings

--- config/settings/production.py
+++ config/settings/production.py
@@ -5,8 +5,8 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-# ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["hintshare.ca"]) + ['hintshare.ca', 'www.hintshare.ca']
-ALLOWED_HOSTS = ["hintshare.ca", "www.hintshare.ca", "hintshare.herokuapp.com"]
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["hintshare.ca"]) 
+# ALLOWED_HOSTS = ["hintshare.ca", "www.hintshare.ca", "hintshare.herokuapp.com"]
 # DATABASES kjbkbk
 # ------------------------------------------------------------------------------
 DATABASES["default"] = env.db("DATABASE_URL")  # noqa F405
