f8f78e66be1038b76da7ca6b423572f7aad9694d
Dilu-Bilu <dilreetraju@gmail.com>
2022-11-14 00:12:08 -0800

    made email method

--- hintshare/users/admin.py
+++ hintshare/users/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth import admin as auth_admin
 from django.contrib.auth import get_user_model
 from django.utils.translation import gettext_lazy as _
-
+from django.core.mail import send_mail
 from hintshare.users.forms import UserAdminChangeForm, UserAdminCreationForm
 
 User = get_user_model()
@@ -30,5 +30,28 @@ class UserAdmin(auth_admin.UserAdmin):
         ),
         (_("Important dates"), {"fields": ("last_login", "date_joined")}),
     )
-    list_display = ["username", "name", "is_superuser"]
-    search_fields = ["name"]
+    list_display = ["username", "name", "secondary_email", "is_superuser"]
+
+    search_fields = ["name", "secondary_email"]
+    actions = ['make_published']
+
+    @admin.action(description='Send promotional emails')
+    def make_published(self, request, queryset):
+        print("Yo")
+        for obj in queryset:
+            sender = "dilreetraju@hintshare.ca"
+            reciever = str(obj.email)
+        
+            subject = "Hintshare Checking Reminder"
+            message = f"There are new questions available on HintShare; log on to check them out."
+            send_mail(subject, message, sender,
+            [reciever], html_message=f"<html>{message} <a href = 'https://hintshare.ca/questions/'>See Questions!</a></html>")
+            print(subject, message, sender)
+            try:
+                reciever2 = str(obj.secondary_email)
+                send_mail(subject, message, sender,
+                [reciever2], html_message=f"<html>{message} <a href = 'https://hintshare.ca/questions/'>See Question!</a></html>")
+            except:
+                pass
+       
+    
