7a128f25e881394adc5262eaa5785d9c9365f310
Dilu-Bilu <dilreetraju@gmail.com>
2022-11-17 00:52:59 -0800

    added robots.txt

--- config/urls.py
+++ config/urls.py
@@ -8,7 +8,7 @@ from django.contrib.sitemaps import GenericSitemap
 from django.contrib.sitemaps.views import sitemap
 from django.urls import path
 from stackbase.models import Question
-
+from django.http import HttpResponse
 info_dict = {
     'queryset': Question.objects.all(),
     'date_field': 'pub_date',
@@ -30,6 +30,10 @@ urlpatterns = [
     path('sitemap.xml', sitemap,
         {'sitemaps': {'questions': GenericSitemap(info_dict, priority=0.6)}},
         name='django.contrib.sitemaps.views.sitemap'),
+    path(
+        "robots.txt",
+        TemplateView.as_view(template_name="robots.txt", content_type="text/plain"),
+    ),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
